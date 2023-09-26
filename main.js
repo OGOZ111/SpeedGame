e228b1f92566b24b3c54d1fcbdb028760aef0036
OGOZ111 <djsmallboy@gmail.com>
2023-09-26 13:49:33 +0300

    add click point events

--- main.js
+++ main.js
@@ -4,19 +4,26 @@ const circles = document.querySelectorAll('.circle')
 const scoreDisplay = document.querySelector('.score')
 
 
+
 // declare global varials
 let score = 0;
 let timer; 
 let pace = 1000;
 let active = 0;
-
+let rounds = 0
 
 
 const randNum = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
 console.log(randNum(0,3));
 
 const clickCircle = (i) => {
-    console.log('Circle was clicked', i)
+    if (i !== active) {
+       return EndGame()
+    }
+
+
+    console.log(i)
+    rounds--
     score += 10
 scoreDisplay.textContent = score    
 }
@@ -25,10 +32,20 @@ circles.forEach((circle, i) => {
     circle.addEventListener('click', () => clickCircle(i))
 })
 
-
+const enableEvents = () => {
+    circles.forEach(circle => {
+        circle.style.pointerEvents ="auto"
+    })
+}
 
 
 const startGame = () => {
+
+    if (rounds >= 3) {
+        return EndGame
+    }
+
+
     const newActive = pickNumber(active) 
 
     circles[newActive].classList.toggle('active')
@@ -37,7 +54,9 @@ const startGame = () => {
 
     active = newActive
     timer = setTimeout(startGame, pace)
+
     pace -= 10
+    rounds++
 
 
 
@@ -54,12 +73,13 @@ const startGame = () => {
 }
 
 const EndGame = () => {
-
-
     clearTimeout(timer)
+    resetgame();
 }
 
-
+const resetgame = () => {
+    window.location.reload()
+}
 
 startbutton.addEventListener('click', startGame)
 endbutton.addEventListener('click', EndGame)
